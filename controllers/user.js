4f20829edbd4121d40cb79847907031ea585b110
Abhi0102 <67204033+Abhi0102@users.noreply.github.com>
2022-03-06 13:10:51 +0530

    User Model Done

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,9 @@
 const BigPromise = require("../middlewares/bigPromise");
 
+exports.signup = BigPromise((req,res)=>{
+  
+})
+
 exports.login = BigPromise((req, res) => {
   res.status(200).json({ success: true });
 });
