deafbb433a3905362b13906b11b37409a1141a65
Abhi0102 <67204033+Abhi0102@users.noreply.github.com>
2022-03-08 16:44:19 +0530

    Landing Page

--- client/src/component/Home.js
+++ client/src/component/Home.js
@@ -1,7 +1,42 @@
+import { Grid, Typography } from "@mui/material";
 import React from "react";
+import { makeStyles } from "@mui/styles";
+import { Box } from "@mui/system";
+
+const useStyles = makeStyles({
+  backImg: {
+    height: "70vh",
+    width: "100vw",
+    backgroundImage: "url('/background.jpg')",
+    backgroundPosition: "center",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+  },
+  introText: {
+    color: "white",
+    margin: "25vh 10vw",
+  },
+});
 
 function Home(props) {
-  return <div>Home</div>;
+  const classes = useStyles();
+  return (
+    <Grid container>
+      <Grid item className={classes.backImg}>
+        <Box className={classes.introText}>
+          <Typography variant="h4">Counsellor</Typography>
+          <br />
+          <Typography variant="h6">
+            A Demo Website Where Users can schedule call with Verified
+            Counsellors and get carrer advice.
+          </Typography>
+        </Box>
+      </Grid>
+      <Grid item mx={5} my={2}>
+        <Typography variant="h4">Our Counsellors</Typography>
+      </Grid>
+    </Grid>
+  );
 }
 
 export default Home;
